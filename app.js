9cb5bdc17cf8bc84b3b0ca7e5ba7fd8809ce81bf
dominic-source <chinonsodomnic@gmail.com>
2023-12-18 14:18:38 +0100

    update and prepare for deployment

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/member",
+  callbackURL: "https://cadaservices.tech/auth/google/member",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
   function (accessToken, refreshToken, profile, cb) {
